f0ae96dcc3bac787954e755049077c4c2d051140
scarlettscoville <scarlettscoville@gmail.com>
2022-04-22 08:22:39 -0400

    added readme

--- blackjack.py
+++ blackjack.py
@@ -5,9 +5,11 @@ from tkinter import *
 root = Tk()
 
 #Creating a Label Widget
-myLabel = Label(root, text = "Dream Team Blackjack")
+myLabel1 = Label(root, text = "Dream Team Blackjack")
+myLabel2 = Label(root, text = "built by Scarlett, Dak, and Grady")
 
 # Shoving it onto the screen
-myLabel.pack()
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=1)
 
 root.mainloop()
