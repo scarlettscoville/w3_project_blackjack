2d4bc06f43b063841400e3e0df9fd4a295f08e3d
scarlettscoville <scarlettscoville@gmail.com>
2022-04-22 08:16:16 -0400

    tkinter added

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,13 @@
 # Let's go, Dream Team!
+
+from tkinter import *
+
+root = Tk()
+
+#Creating a Label Widget
+myLabel = Label(root, text = "Dream Team Blackjack")
+
+# Shoving it onto the screen
+myLabel.pack()
+
+root.mainloop()
