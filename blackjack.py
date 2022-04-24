c02d99415a596a68ff5d54815ddc933f7ab9e904
Dakodah Bradley <comatose9592@gmail.com>
2022-04-24 14:34:51 -0500

    player_ui updates

--- blackjack.py
+++ blackjack.py
@@ -18,7 +18,7 @@ dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
 dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
 
 player_frame = LabelFrame(my_frame, text="Player", bd=0)
-player_frame.grid(row=0, column=1, ipadx=20)
+player_frame.grid(row=1, column=0, ipadx=20, pady=20,)
 
 dealer_label = Label(dealer_frame, text='')
 dealer_label.pack(pady=20)
@@ -29,7 +29,7 @@ player_label.pack(pady=20)
 shuffle_button = Button(root, text="Shuffle Deck", font=("Ariel", 14))
 shuffle_button.pack(pady=20)
 
-hit_button = Button(root, text="Hit", font=("Ariel", 14))
+hit_button = Button(root, text="Hit", font=("Ariel", 14), command=hit_me)
 hit_button.pack(pady=20)
 
 
@@ -42,9 +42,56 @@ class Dealer():
         pass
     
     def hit_me():
-        # if sum of two cards is 16 and below, hit
-            # if 17 or above, pass
-        pass
+        global dealer_spot
+        if dealer_spot < 5:
+            try:
+                dealer_card = random.choice(deck)
+                deck.remove(dealer_card)
+                dealer.append(dealer_card)
+                global dealer_image1, dealer_image2, dealer_image3, dealer_image4, dealer_image5
+                
+                if dealer_spot == 0:
+                    # Resize Card
+                    dealer_image = resize_cards(f'images/cards/{dealer_card}.png')
+                    # Output card to screen    
+                    dealer_label_1.config(image=dealer_image)
+                    # Increment dealer card by 1
+                    dealer_spot += 1
+                elif dealer_spot == 1:
+                    # Resize Card
+                    dealer_image1 = resize_cards(f'images/cards/{dealer_card}.png')
+                    # Output dealer_card to screen    
+                    dealer_label_1.config(image=dealer_image1)
+                    # Increment dealer card by 1
+                    dealer_spot += 1
+                elif dealer_spot == 2:
+                    # Resize Card
+                    dealer_image2 = resize_cards(f'images/cards/{dealer_card}.png')
+                    # Output card to screen    
+                    dealer_label_2.config(image=dealer_image2)
+                    # Increment dealer card by 1
+                    dealer_spot += 1
+                elif dealer_spot == 3:
+                    # Resize Card
+                    dealer_image3 = resize_cards(f'images/cards/{dealer_card}.png')
+                    # Output card to screen    
+                    dealer_label_3.config(image=dealer_image3)
+                    # Increment dealer card by 1
+                    dealer_spot += 1
+                elif dealer_spot == 4:
+                    # Resize Card
+                    dealer_image4 = resize_cards(f'images/cards/{dealer_card}.png')
+                    # Output card to screen    
+                    dealer_label_4.config(image=dealer_image4)
+                    # Increment dealer card by 1
+                    dealer_spot += 1
+
+                root.title(f'Dream Team Blackjack - {len(deck)} Cards Left')
+
+
+
+            except:
+                root.title(f'Dream Team Blackjack')
 
     def show_cards():
         pass
@@ -72,9 +119,9 @@ class Deck():
     # holds our card values
     
     def shuffle_cards():
-        pass
-       
+        # Shuffle 2 cards for player and dealer
         
+       
 
     def deal_cards():
         pass
@@ -87,9 +134,9 @@ class Player_UI():
         # 1st - option to start game upon opening program
         play_game = input("Would you like to begin? [Y / N] ").capitalize()
         if play_game == "Y":
-            
+            Player_UI.start_game() # Recurrsive call
         # graphics menu
-        pass
+        
     
     def start_hand():
         # -bets 
@@ -99,6 +146,57 @@ class Player_UI():
         pass
     
     def hit_me():
+        global player_spot
+        if player_spot < 5:
+            try:
+                player_card = random.choice(deck)
+                deck.remove(player_card)
+                player.append(player_card)
+                global player_image1, player_image2, player_image3, player_image4, player_image5
+                
+                if player_spot == 0:
+                    # Resize Card
+                    player_image = resize_cards(f'images/cards/{player_card}.png')
+                    # Output card to screen    
+                    player_label_1.config(image=player_image)
+                    # Increment player card by 1
+                    player_spot += 1
+                elif player_spot == 1:
+                    # Resize Card
+                    player_image1 = resize_cards(f'images/cards/{player_card}.png')
+                    # Output card to screen    
+                    player_label_1.config(image=player_image1)
+                    # Increment player card by 1
+                    player_spot += 1
+                elif player_spot == 2:
+                    # Resize Card
+                    player_image2 = resize_cards(f'images/cards/{player_card}.png')
+                    # Output card to screen    
+                    player_label_2.config(image=player_image2)
+                    # Increment player card by 1
+                    player_spot += 1
+                elif player_spot == 3:
+                    # Resize Card
+                    player_image3 = resize_cards(f'images/cards/{player_card}.png')
+                    # Output card to screen    
+                    player_label_3.config(image=player_image3)
+                    # Increment player card by 1
+                    player_spot += 1
+                elif player_spot == 4:
+                    # Resize Card
+                    player_image4 = resize_cards(f'images/cards/{player_card}.png')
+                    # Output card to screen    
+                    player_label_4.config(image=player_image4)
+                    # Increment player card by 1
+                    player_spot += 1
+
+                root.title(f'Dream Team Blackjack - {len(deck)} Cards Left')
+
+
+
+            except:
+                root.title(f'Dream Team Blackjack')
+
         # input: Do you want to hit or pass?
         # will work before the dealer.hit_me
         pass
