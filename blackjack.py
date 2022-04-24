c824f8d87dd51b061cdc2dee1cf93c7e3162b202
scarlettscoville <scarlettscoville@gmail.com>
2022-04-24 12:01:41 -0400

    updates

--- blackjack.py
+++ blackjack.py
@@ -1,42 +1,15 @@
 # Let's go, Dream Team!
-
-
-import random
 from tkinter import *
+import random
+from PIL import Image, ImageTk
+from tkinter import messagebox
 
 root = Tk()
 root.title('Dream Team Blackjack')
 root.iconbitmap('./images/Blackjack project icon.png')
-root.geometry("900x500")
+root.geometry("1200x800")
 root.configure(background="green")
-
-
-my_frame = Frame(root, bg="green")
-my_frame.pack(pady=20)
-
-dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
-dealer_frame.grid(row=0, column=0, padx=20, ipadx=20)
-
-player_frame = LabelFrame(my_frame, text="Player", bd=0)
-player_frame.grid(row=0, column=1, ipadx=20)
-
-dealer_label = Label(dealer_frame, text='')
-dealer_label.pack(pady=20)
-
-player_label = Label(player_frame, text='')
-player_label.pack(pady=20)
-
-shuffle_button = Button(root, text="Shuffle Deck", font=("Ariel", 14))
-shuffle_button.pack(pady=20)
-
-hit_button = Button(root, text="Hit", font=("Ariel", 14))
-hit_button.pack(pady=20)
-
-
-root.mainloop()
-#------------------------GAME--------------------------------------#
-
-
+#--------------------START GAME CODE-----------------------------------------#
 class Dealer():
     def __init__(self):
         pass
@@ -116,6 +89,74 @@ class Player_UI():
         # will work before the dealer.hit_me
         pass
 
-    # Driver Code #
+
+#-----------------------------------------------------------------#
+#--------------START TKINTER FOOTER CODE--------------------------#
+my_frame = Frame(root, bg="green")
+my_frame.pack(pady=20)
+
+# Create Frames For Cards
+dealer_frame = LabelFrame(my_frame, text="Dealer", bd=0)
+dealer_frame.pack(padx=20, ipadx=20)
+
+player_frame = LabelFrame(my_frame, text="Player", bd=0)
+player_frame.pack(ipadx=20, pady=10)
+
+# Put Dealer cards in frames
+dealer_label_1 = Label(dealer_frame, text='')
+dealer_label_1.grid(row=0, column=0, pady=20, padx=20)
+
+dealer_label_2 = Label(dealer_frame, text='')
+dealer_label_2.grid(row=0, column=1, pady=20, padx=20)
+
+dealer_label_3 = Label(dealer_frame, text='')
+dealer_label_3.grid(row=0, column=2, pady=20, padx=20)
+
+dealer_label_4 = Label(dealer_frame, text='')
+dealer_label_4.grid(row=0, column=3, pady=20, padx=20)
+
+dealer_label_5 = Label(dealer_frame, text='')
+dealer_label_5.grid(row=0, column=4, pady=20, padx=20)
+
+# Put Player cards in frames
+player_label_1 = Label(player_frame, text='')
+player_label_1.grid(row=1, column=0, pady=20, padx=20)
+
+player_label_2 = Label(player_frame, text='')
+player_label_2.grid(row=1, column=1, pady=20, padx=20)
+
+player_label_3 = Label(player_frame, text='')
+player_label_3.grid(row=1, column=2, pady=20, padx=20)
+
+player_label_4 = Label(player_frame, text='')
+player_label_4.grid(row=1, column=3, pady=20, padx=20)
+
+player_label_5 = Label(player_frame, text='')
+player_label_5.grid(row=1, column=4, pady=20, padx=20)
+
+# Create Button Frame
+button_frame = Frame(root, bg="green")
+button_frame.pack(pady=20)
+
+# Create a couple buttons
+shuffle_button = Button(button_frame, text="Shuffle Deck", font=("Helvetica", 14), command=shuffle)
+shuffle_button.grid(row=0, column=0)
+
+card_button = Button(button_frame, text="Hit Me!", font=("Helvetica", 14), command=player_hit)
+card_button.grid(row=0, column=1, padx=10)
+
+stand_button = Button(button_frame, text="Stand!", font=("Helvetica", 14), command=stand)
+stand_button.grid(row=0, column=2)
+
+
+
+# Shuffle Deck On Start
+shuffle()
+
+
+root.mainloop()
+
+
+# ---------Driver Code-----------#
 #if __name__ == '__main__':
 #    Game.play_game() ##Need to adjust for this game.
