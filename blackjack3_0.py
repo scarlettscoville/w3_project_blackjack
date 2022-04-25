76fba2f62a0774e597e9eb2b1811088e0412efe7
scarlettscoville <scarlettscoville@gmail.com>
2022-04-24 20:35:28 -0400

    adding file

---
+++ blackjack3_0.py
@@ -0,0 +1,23 @@
+## Classes:
+    # Deck
+    # Card
+    # Player
+    # Table
+
+import random
+
+class Deck:
+    def __init__(self):
+    pass
+
+class Card:
+    def __init__(self):
+    pass
+
+class Player:
+    def __init__(self):
+    pass
+
+class Table_UI:
+    def __init__(self):
+    pass
