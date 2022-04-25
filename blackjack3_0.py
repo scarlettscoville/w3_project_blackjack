29bceeb8876e4c2a8a65feb9b02a0367869767b9
Dakodah Bradley <comatose9592@gmail.com>
2022-04-25 14:34:59 -0500

    Can't get it to replay

--- blackjack3_0.py
+++ blackjack3_0.py
@@ -23,15 +23,13 @@ class Card:
         self.suit = suit
         self.value = value
         
-        
-
     def __repr__(self):     # Able to customize the string representation of the object. By default the output contains the memory address and the __repr__ addresses this issue
-         return f'{self.value} of  {self.suit}'
+         return f'{self.value} {self.suit}'
 
 class Deck:
 
     def __init__(self):
-        self.cards = [Card(suit, value) for suit in ["Spades", "Clubs", "Hearts", "Diamonds"] for value in
+        self.cards = [Card(suit, value) for suit in ["♠️", "♣", "♥", "♦"] for value in
                     ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]]
         
     def shuffle_deck(self):
@@ -48,8 +46,6 @@ class Player:
         self.dealer = dealer
         self.cards = []
         self.value = 0
-        self.rank = '♥♦♣♠'
-        
         
     def add_card(self, card):
         self.cards.append(card)
@@ -79,8 +75,14 @@ class Player:
             print(self.cards[1])
         else:
             for card in self.cards:
-                print(card)
-            print("Value:", self.get_value())
+                print('┌───────┐')
+                print('| Dream |')
+                print('|       |')
+                print(f'|  {card}  |')
+                print('|       |')
+                print('| Team  |')
+                print('└───────┘') 
+        print("Value:", self.get_value())
 
 class Table():
     def __init__(self):
@@ -102,8 +104,11 @@ class Table():
 
             print("Here is what you have: ")
             self.player_hand.display()
-            print("------------$$-------------")
-            print("------------$$-------------")
+            print()
+            print("♠️", "♣", "♥", "♦","♠️", "♣", "♥", "♦","♠️", "♣", "♥", "♦")
+            print("------------$$---------")
+            print("♠️", "♣", "♥", "♦","♠️", "♣", "♥", "♦","♠️", "♣", "♥", "♦")
+            print()
             print("Here is the Dealer's Hand: ")
             self.dealer_hand.display()
             print()
@@ -144,14 +149,14 @@ class Table():
 
                     game_over = True
 
+            play_again = input("Would you like to lose more money? [Y / N] ").capitalize()
+            while play_again.capitalize() not in ["Y", "N"]:
                 play_again = input("Would you like to lose more money? [Y / N] ").capitalize()
-                while play_again.capitalize() not in ["Y", "N"]:
-                    play_again = input("Would you like to lose more money? [Y / N] ").capitalize()
-                if play_again.capitalize() == "N":
-                    print("Thanks for playing!")
-                    playing = False
-                else:
-                    game_over = False
+            if play_again.capitalize() == "N":
+                print("Thanks for playing! 🤑🤑🤑")
+                playing = False
+            else:
+                game_over = False
     
     def player_hand_over(self):
         return self.player_hand.get_value() > 21
@@ -168,11 +173,11 @@ class Table():
 
     def show_blackjack(self, player_has_blackjack, dealer_has_blackjack):
         if player_has_blackjack and dealer_has_blackjack:
-            print("Both players have blackjack! Draw!")
+            print("It looks like a draw partner! 🤠🤠🤠 ")
         elif player_has_blackjack:
-            print("You have Blackjack, You Win!")
+            print("You have Blackjack, You Win! 💸")
         elif dealer_has_blackjack:
-            print("Dealer has Blackjack, Dealer Wins!")
+            print("Dealer has Blackjack, Dealer Wins! 🫴🫴")
 
 if __name__ == "__main__":
     test = Table()
