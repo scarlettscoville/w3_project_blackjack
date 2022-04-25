182e7c7c8af5e08e4ce9eb33cbb72f31caa6bbc1
scarlettscoville <scarlettscoville@gmail.com>
2022-04-24 20:51:56 -0400

    let's go\

--- blackjack3_0.py
+++ blackjack3_0.py
@@ -8,16 +8,16 @@ import random
 
 class Deck:
     def __init__(self):
-    pass
+        pass
 
 class Card:
     def __init__(self):
-    pass
+        pass
 
 class Player:
     def __init__(self):
-    pass
+        pass
 
 class Table_UI:
     def __init__(self):
-    pass
+        pass
