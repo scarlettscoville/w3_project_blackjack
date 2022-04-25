f8691a22ea0f7a6236296715b75bfb73e83694bc
Dakodah Bradley <comatose9592@gmail.com>
2022-04-24 22:51:20 -0500

    tasks added

--- blackjack3_0.py
+++ blackjack3_0.py
@@ -3,7 +3,18 @@
     # Card
     # Player
     # Table
-
+    
+# Tasks
+    # Logic who states who wins
+        # If-Else for bust logic
+    # Finishing out which cards indicate which values (K,Q,J,A)
+    # Hit Me Function
+        # Stand function
+    # Dealer hits if value below 16
+    # Running Score
+    # Dealer Hand
+    # Create a method if the player got Blackjack on the first round (if val == 21 return BlackJack)
+    # 
 import random
 
 class Deck():
@@ -41,11 +52,12 @@ class Card(Deck):
 
 class Player(Deck):
 
-    def __init__(self, name):
+    def __init__(self, name, dealer=False):
         self.name = name
         self.hand = []
+        self.dealer = dealer
+        self.value = 0
         
-
     def draw_card(self, deck):
         self.hand.append(deck.draw_card())
         return self
@@ -56,9 +68,30 @@ class Player(Deck):
             self.draw_card(deck)
         return hand
 
+    def dealer_display(self):
+        if self.dealer:
+            print("Hidden")
+            print(self.hand[1])
+
     def show_hand(self):
         for card in self.hand:
             card.show()
+
+    # Need to test
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.hand:
+            if card.value.isnumeric():
+                self.value += int(card.value)
+            else:
+                if card.value == "A":
+                    has_ace = True
+                    self.value += 11
+                else:
+                    self.value += 10
+        if has_ace and self.value > 21:
+            self.value -= 10
 
 class Table_UI(Deck):
     def __init__(self, player):
@@ -69,6 +102,9 @@ class Table_UI(Deck):
         if user_opt == "Y":
             player_hand = self.player.deal_card()
             self.player.show_hand()
+            dealer_hand = self.player.deal_card()
+            
+
 
             
 
